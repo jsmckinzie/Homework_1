cd7ce6d7518b77bf43c2a80b7491d75e7b78052b
jsmckinzie <joelmckinzie@hotmail.com>
2015-09-13 15:24:06 -0500

    Fixed file paths and permissions

--- Trash.sh
+++ Trash.sh
@@ -1,3 +1,7 @@
+#Joel McKinzie: ME701 Fall 2015
+#This program takes the name of a file in the current working directory
+#It then moves the file to a folder named Trash in my home directory
+#If the directory does not exist, the script creates it
 
 echo "Ensure you use the 'sudo' call before ./Trash.sh" 
 echo -n "Enter the name of the File you would like to move to trash: "
@@ -5,12 +9,14 @@ read lFile
 
 if [ -e "$lFile" ]
 then
-	if [ -d "/home/Trash" ]
+	if [ -d "/home/jsmckinzie/Trash" ]
 	then
-		mv $lFile "/home/Trash"
-	else
-		mkdir "/home/Trash"
-		mv $lFile "/home/Trash"
+		mv $lFile "/home/jsmckinzie/Trash"
+	else 
+		echo "Inside else"
+		mkdir -p "/home/jsmckinzie/Trash"
+		chmod o+w "/home/jsmckinzie/Trash"
+		mv $lFile "/home/jsmckinzie/Trash"
 	fi
 else
 	echo "The file you are trying to move does not exist!"
